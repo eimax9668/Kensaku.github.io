dd21fe4cef8bdca02881ee919919631fd82a73d9
ei-max <eima.tanabe@gmail.com>
2024-06-15 20:46:43 +0900

    時計変更

--- time.js
+++ time.js
@@ -1,9 +1,19 @@
-function showClock1() {
-var nowTime = new Date();
-     var nowHour = nowTime.getHours();
-     var nowMin = nowTime.getMinutes();
-     var nowSec = nowTime.getSeconds();
-     var msg = nowHour + ":" + nowMin;
- document.getElementById("ClockArea").innerHTML = msg;
-    }
-    setInterval('showClock1()',1000);
+// script.js
+function setClock() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    const minuteDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+    const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+    
+    document.getElementById('second').style.transform = `rotate(${secondDegrees}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${minuteDegrees}deg)`;
+    document.getElementById('hour').style.transform = `rotate(${hourDegrees}deg)`;
+  }
+  
+  setInterval(setClock, 1000);
+  setClock(); // 初回実行
+  
