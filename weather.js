e49024b9f57b08f46d4d7d95fd9aecac976d049d
ei-max <eima.tanabe@gmail.com>
2024-06-16 18:57:29 +0900

    天気はローカルに保存されます

--- weather.js
+++ weather.js
@@ -1,11 +1,60 @@
-// APIエンドポイントのURL
-const apiUrl = 'https://weather.tsukumijima.net/api/forecast/city/130010';
+let cityNumbers = ["130010", "040010", "230010","390010","400040"];
+let areaNames = ["東京", "仙台", "名古屋","高知","福岡"];
+//それぞれ、東京、仙台、名古屋、高知、福岡。
+//インデックスは0始まり。福岡→4
 
-// APIからデータを取得
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => displayTodayWeather(data))
-    .catch(error => console.error('Error:', error));
+let index = 0;
+
+window.onload;{
+    loadarea();
+    fetchdata();
+}
+
+function savearea(){
+    localStorage.setItem("area",index);
+    let savedindex =localStorage.getItem("area");
+    console.log(savedindex);
+console.log("保存Done");
+}
+
+function loadarea(){
+    let savedindex =localStorage.getItem("area");
+    index = Number(savedindex);
+    if(index > 4 || index < 0){
+        resetarea()
+    }
+}
+
+function resetarea(){
+    index = 0
+    localStorage.clear();
+    alert("エラー。天気の位置情報はリセットされました。");
+}
+
+
+document.querySelector('#changearea').addEventListener("click", function(event){
+    console.log(event)
+    event.preventDefault();
+    fetchdata();
+});
+
+function fetchdata() {
+    console.log(index);
+
+    // APIエンドポイントのURL
+    let apiUrl = "https://weather.tsukumijima.net/api/forecast/city/" + cityNumbers[index];
+
+    // APIからデータを取得
+    //一回消しておく
+    document.getElementById("weather-forecast").innerHTML = "";
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayTodayWeather(data))
+        .catch(error => console.error('Error:', error));
+    console.log(apiUrl)
+    let areaName = areaNames[index];
+    document.getElementsByClassName('weather-area')[0].innerHTML = areaName;
+}
 
 // 今日の天気をHTMLに表示する関数
 function displayTodayWeather(data) {
@@ -19,9 +68,9 @@ function displayTodayWeather(data) {
     forecastDiv.classList.add('forecast');
 
     // 日付と天気
-    const dateLabel = document.createElement('h3');
-    dateLabel.textContent = todayForecast.dateLabel + ' (' + todayForecast.date + ')';
-    forecastDiv.appendChild(dateLabel);
+    //const dateLabel = document.createElement('h3');
+    //dateLabel.textContent = todayForecast.dateLabel + ' (' + todayForecast.date + ')';
+    //forecastDiv.appendChild(dateLabel);
 
     // 天気画像
     const weatherImage = document.createElement('img');
@@ -33,7 +82,13 @@ function displayTodayWeather(data) {
     const weatherDesc = document.createElement('p');
     weatherDesc.textContent = todayForecast.telop;
     forecastDiv.appendChild(weatherDesc);
+    weatherDesc.classList.add("weatherDesc");
 
     // forecastDivをweatherForecastに追加
     weatherForecast.appendChild(forecastDiv);
+    savearea();
+    index = index + 1
+    if (index == 5) {
+        index = 0;
+    }
 }
