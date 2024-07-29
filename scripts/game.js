700231176728a6252c6f4c102b1503c192ddcb70
ei-max <eima.tanabe@gmail.com>
2024-07-29 12:39:12 +0900

    超Bigな更新

---
+++ scripts/game.js
@@ -0,0 +1,23 @@
+function playgame(url,element){
+    fetch(`${url}`)
+    .then((response) => response.text())
+    .then((data) => document.querySelector(`${element}`).innerHTML = data);
+  }
+document.querySelector("#cleanthesea-play").addEventListener("click",function(event){
+    event.preventDefault();
+    playgame("games/cleanthesea.html","#cleanthesea");
+})
+document.querySelector("#irairagame-play").addEventListener("click",function(event){
+    event.preventDefault();
+    playgame("games/irairagame.html","#irairagame");
+})
+
+document.querySelector("#cleanthesea-stop").addEventListener("click",function(event){
+    event.preventDefault();
+    document.querySelector("#cleanthesea").innerHTML = '<img class="gameimg" src="/gameimg/cleanthesea.png" />' ;
+})
+
+document.querySelector("#irairagame-stop").addEventListener("click",function(event){
+    event.preventDefault();
+    document.querySelector("#irairagame").innerHTML = '<img class="gameimg" src="/gameimg/irairagame.png" />' ;
+})
